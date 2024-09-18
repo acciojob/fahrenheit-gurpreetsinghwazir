89ac47856cb83cca6670a683182854b29c34421b
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-09-18 21:53:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function toFahrenheit(celsius) { 
  // Write your code here
 	let sum = (+celsius * 9/5) + 32
-
-return (sum)
+	return (sum.toFixed(2))
 }
-
+ 
 
 // Do not change the code below
 const celsius = prompt("Enter Celsius:");
